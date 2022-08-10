0d9ca7db4ae90cee1bd2ec9a0e08047481b38a0a
LukaZukhbaia <95714793+LukaZukhbaia@users.noreply.github.com>
2022-08-10 17:22:33 +0400

    Added image icon in PDF.

--- files/scripts/main.py
+++ files/scripts/main.py
@@ -39,11 +39,15 @@ class PdfReport:
         self.filename = filename
 
     def generate(self, flatmate1, flatmate2, bill):
+        flatmate1_pay = str(round(flatmate1.pays(bill, flatmate2), 2))
+        flatmate2_pay = str(round(flatmate2.pays(bill, flatmate1), 2))
+
         pdf = FPDF(orientation='P', unit='pt', format="A4")
         pdf.add_page()
 
-        flatmate1_pay = str(round(flatmate1.pays(bill, flatmate2), 2))
-        flatmate2_pay = str(round(flatmate2.pays(bill, flatmate1), 2))
+        # Add Icon
+        pdf.image("../house.png", w=30, h=30)
+
         # Add some info
         pdf.set_font(family="Times", size=24, style="B")
         pdf.cell(w=0, h=80, txt="Flatmates Bill", border=1, align="C", ln=1)
