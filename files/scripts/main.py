bf3078c44ad970f357497f8fea22afb506f0d386
LukaZukhbaia <95714793+LukaZukhbaia@users.noreply.github.com>
2022-08-10 17:52:10 +0400

    made PDF better-looking.

--- files/scripts/main.py
+++ files/scripts/main.py
@@ -50,16 +50,20 @@ class PdfReport:
 
         # Add some info
         pdf.set_font(family="Times", size=24, style="B")
-        pdf.cell(w=0, h=80, txt="Flatmates Bill", border=1, align="C", ln=1)
+        pdf.cell(w=0, h=80, txt="Flatmates Bill", border=0, align="C", ln=1)
+
+        pdf.set_font(family="Times", size=14, style="B")
         # Insert Period - value
-        pdf.cell(w=100, h=40, txt="Period:", border=1)
-        pdf.cell(w=150, h=40, txt=bill.period, border=1, ln=1)
+        pdf.cell(w=100, h=40, txt="Period:", border=0)
+        pdf.cell(w=150, h=40, txt=bill.period, border=0, ln=1)
+
+        pdf.set_font(family="Times", size=12)
         # Insert First Flatmate
-        pdf.cell(w=100, h=40, txt=flatmate1.name, border=1)
-        pdf.cell(w=150, h=40, txt=flatmate1_pay, border=1, ln=1)
+        pdf.cell(w=100, h=24, txt=flatmate1.name, border=0)
+        pdf.cell(w=150, h=24, txt=flatmate1_pay, border=0, ln=1)
         # Insert Second Flatmate
-        pdf.cell(w=100, h=40, txt=flatmate2.name, border=1)
-        pdf.cell(w=150, h=40, txt=flatmate2_pay, border=1, ln=1)
+        pdf.cell(w=100, h=24, txt=flatmate2.name, border=0)
+        pdf.cell(w=150, h=24, txt=flatmate2_pay, border=0, ln=1)
 
         pdf.output(self.filename)
 
